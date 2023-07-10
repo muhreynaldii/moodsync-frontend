35fa46e6ec06b2412d881cd59f56fb3ea63fe3d4
muhreynaldii <reynaldi180101@gmail.com>
2023-07-10 18:24:16 +0700

    add random session

--- components/PopUpJoin.vue
+++ components/PopUpJoin.vue
@@ -16,7 +16,7 @@
         >
         <input
           v-model="itsUserName"
-          class="mb-4 h-[67.03px] w-[500.92px] focus:outline-none focus:border-blue-500 focus:ring-blue-500 rounded-[13px] border-2 border-[#D1D5DB] px-[26px] py-[19px] text-[22.77px]"
+          class="mb-4 h-[67.03px] w-[500.92px] rounded-[13px] border-2 border-[#D1D5DB] px-[26px] py-[19px] text-[22.77px] focus:border-blue-500 focus:outline-none focus:ring-blue-500"
           type="text"
           placeholder="Masukkan Nama Lengkap"
           required
@@ -27,15 +27,21 @@
         >
         <input
           v-model="itsSessionId"
-          class="mb-4 h-[67.03px] w-[500.92px] focus:outline-none focus:border-blue-500 focus:ring-blue-500 rounded-[13px] border-2 border-[#D1D5DB] px-[26px] py-[19px] text-[22.77px]"
+          class="mb-4 h-[67.03px] w-[500.92px] rounded-[13px] border-2 border-[#D1D5DB] px-[26px] py-[19px] text-[22.77px] focus:border-blue-500 focus:outline-none focus:ring-blue-500"
           type="text"
           placeholder="Masukkan Kode Ruangan"
           required
         />
         <p class="text-center">
-          <button class="mx-2 h-[48px] w-[119px] rounded-[8px] border border-gray-200 bg-white hover:border-red-400 hover:bg-red-500 text-black hover:text-white mt-2" @click="goBack">Kembali</button>
-          <button 
-            class="mx-2 h-[48px] w-[119px] rounded-[8px] bg-blue-600 hover:bg-blue-700 text-white">
+          <button
+            class="mx-2 mt-2 h-[48px] w-[119px] rounded-[8px] border border-gray-200 bg-white text-black hover:border-red-400 hover:bg-red-500 hover:text-white"
+            @click="goBack"
+          >
+            Kembali
+          </button>
+          <button
+            class="mx-2 h-[48px] w-[119px] rounded-[8px] bg-blue-600 text-white hover:bg-blue-700"
+          >
             Join!
           </button>
         </p>
@@ -46,6 +52,9 @@
 
 <script>
 export default {
+  mounted() {
+    this.generateRandomMeetingCode();
+  },
   data() {
     return {
       itsUserName: this.$auth.user || "",
@@ -59,14 +68,23 @@ export default {
     goBack() {
       this.$router.go(-1); // Kembali ke halaman sebelumnya
     },
+    generateRandomMeetingCode() {
+      var characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+      var meetingCode = "";
+
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+          meetingCode += characters.charAt(
+            Math.floor(Math.random() * characters.length)
+          );
+        }
+        if (i < 2) {
+          meetingCode += "-";
+        }
+      }
+
+      this.itsSessionId = meetingCode;
+    },
   },
-  // props: {
-  //   itsUserName: {
-  //     type: String,
-  //   },
-  //   itsSessionId: {
-  //     type: String,
-  //   },
-  // },
 };
 </script>
